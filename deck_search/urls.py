a558b241378910a57176d066705ff3f227e865ac
jms1192 <62626527+jms1192@users.noreply.github.com>
2020-06-06 10:45:47 -0400

    Add files via upload

--- deck_search/urls.py
+++ deck_search/urls.py
@@ -1,21 +1,23 @@
 from django.urls import path, include
 from . import views
+from .views import search_deck1, deck_display1, deck_page1
 #this file takes urls from the web and redirects them to the views that will exicue a function baced on the url entered
 urlpatterns = [
-    path('search_deck/', views.search_deck),
-    path('deck_page/<id>', views.deck_page),
+    path('search_deck/', search_deck1.as_view()),
+    path('deck_page/<id>', deck_page1.as_view()),
     path('copy_deck/<id>', views.copy_deck),
     path('new_deck/', views.new_deck),
     path('new_deck_name/<type1>', views.new_deck_name),
     path('delete_deck/<id>', views.delete_deck),
     path('publish_deck/<id>', views.publish_deck_v),
     path('sample_hand/<id>', views.sample_hand),
-    path('deck_display/<hash_tags>', views.deck_display),
+    path('deck_display/<page>/<hash_tags>', deck_display1.as_view()),
     path('popular_decks/', views.popular_decks),
     path('make_commander/<id_c>/<id_d>', views.make_commander),
     path('add_sideboard/<id_c>/<id_d>', views.add_sideboard),
     path('delete_card/<id_c>/<id_d>', views.remonve_card_from_deck),
-    path('move_from_sideboard/<id_c>/<id_d>', views.from_sideboard_to_deck)
+    path('move_from_sideboard/<id_c>/<id_d>', views.from_sideboard_to_deck),
+    path('deck_analysis/<d_id>', views.deck_analysis)
     
 
     
--- deck_search/views.py
+++ deck_search/views.py
@@ -3,132 +3,172 @@ from home.models import decks, cards_in_deck, UserProfile, cards
 from .forms import add_deck, publish_deck, deck_searchform, add_card_to_deck
 import collections
 import random
+from django.views import View
 
 #this loads the data for the deck search page 
-def search_deck(request):
 
-    if request.method == 'POST':
+class search_deck1(View):
+
+    def get(self,request):
+        form = deck_searchform()
+
+        if request.user.is_authenticated == True:
+            return render(request,'deck_search/user_search.html', {'form':form})
+        else:
+            return render(request, 'deck_search/search.html', {'form':form})
+    
+    def post(self, request):
         form = deck_searchform(request.POST)
         if form.is_valid():
             
             hash_tags = form.cleaned_data.get("hash_tags")
 
-            link = 'http://localhost:8000/deck_search/deck_display/' + (str(hash_tags))[1:]
+            link = 'http://localhost:8000/deck_search/deck_display/1/' + (str(hash_tags))[1:]
             return redirect(link)
-
-    form = deck_searchform()
-
-    if request.user.is_authenticated == True:
-        return render(request,'deck_search/user_search.html', {'form':form})
-    else:
-        return render(request, 'deck_search/search.html', {'form':form})
 
 
 #this loads the data for the results of the search page 
-def deck_display(request, hash_tags):
-    deck_list = decks.objects.filter(deck_publish=True)
 
-    tag_list = hash_tags.split('#')
-    for i in tag_list:
-        deck_list = deck_list.filter(hash_tags__icontains=i)
-        deck_list = deck_list.extra( order_by = ['-deck_copies'])
+class deck_display1(View):
+    def get(self, request, page, hash_tags):
+        deck_list = decks.objects.filter(deck_publish=True)
 
-    if request.user.is_authenticated == True:
-        return render(request,'deck_search/user_deck_display.html',{'deck_list': deck_list})
-    else:
-        return render(request,'deck_search/deck_display.html', {'deck_list': deck_list})
+        tag_list = hash_tags.split('#')
+        for i in tag_list:
+            deck_list = deck_list.filter(hash_tags__icontains=i)
+            deck_list = deck_list.extra( order_by = ['-deck_copies'])
+
+        if len(deck_list) > 30:
+            number_pages = math.ceil((len(deck_page))/30)
+            page_list = []
+            for p in range(number_pages):
+                if p == number_pages:
+                    page_cards = deck_list[((p-1) * 30):]
+                    next_page = False 
+                else:
+                    page_cards = deck_list[((p-1) * 30):(p * 30)]
+                    next_page = True
+                page_list.append(page_cards)
+                current_page = page_list[(page-1)]
+
+            if page == 1:
+                previous_page = False
+                next_page = True
+            elif page == number_pages:
+                previous_page = True
+                next_page = False
+            else:
+                previous_page = True
+                next_page = True         
+
+        else:
+            current_page = deck_list
+            previous_page = False
+            next_page = False
+            number_pages = 1
+
+
+
+        if request.user.is_authenticated == True:
+            return render(request,'deck_search/user_deck_display.html',{'deck_list': current_page, 'previous_page':previous_page, 'number_pages':number_pages, 'next_page':next_page, 'hash_tags':hash_tags, 'page':page})
+        else:
+            return render(request,'deck_search/deck_display.html', {'deck_list': current_page, 'previous_page':previous_page, 'number_pages':number_pages, 'next_page':next_page, 'hash_tags':hash_tags, 'page':page})
 
 
 #this loads the data for a deck pages on if the users is loged in and what kind of deck it is 
-def deck_page(request,id):
-    deck = decks.objects.filter(id = id)
-    card2 = cards_in_deck.objects.filter(deck = deck[0]).exclude(is_commander = True).exclude(is_sideboard=True)
-    hash_tags1 =[i.hash_tags for i in deck] 
-    tags = list([(i[1:]).split('#') for i in hash_tags1])
-    if len(tags) == 1:
-        tags = tags[0]
-    card1 = []
-    for i in card2:
-        j = i.card
-        card1.append(j)
-    
-    card3 = list((collections.Counter(card1)).items())
-    #can change type list to any othey card atribute to change how the cards are sorted
-    type_list = [x[0].type1 for x in card3]
-    type_list = list(dict.fromkeys(type_list))
-    #sort_list = sort_function 
 
-    cards_split = []
-    for i in type_list:
-        x = [x for x in card3 if x[0].type1 == i]
-        y = sum([y[1] for y in x ])
-        cards_split.append([i,y,x])
-    #broken up baced on kind of could possibly be more efficent 
-    deck_kind = deck[0].deck_type
-    if deck[0].deck_type == 'Commander':
-        try:
-            commander =  (cards_in_deck.objects.filter(deck = deck[0]).filter(is_commander = True))[0].card
-        except:
-            commander = 2
-        sideboard = 1
-        context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':commander, 'sideboard': sideboard }
-
-
-    elif (deck[0]).deck_type == 'Constructed':
-        try:
-            side = [(i.card) for i in cards_in_deck.objects.filter(deck = deck[0]).filter(is_sideboard= True)]
-            sideboard = list((collections.Counter(side)).items())
-
-        except:
-            sideboard = 2
-        commander = 1
-        context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':commander, 'sideboard': sideboard }
-
-    else:
-        sideboard = 1
-        commander = 1
-        context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':1, 'sideboard': sideboard }
-
-    try:
-        user = UserProfile.objects.filter(user = request.user)[0]
-    except:
-        user = 1
-
-    deck1 = (decks.objects.get(id=id))
-    if not user == deck1.deck_creator:
-        deck1.deck_views = deck1.deck_views + 1
-        deck1.save(update_fields=['deck_views'])
-
-    if request.user.is_authenticated == True:
-        if UserProfile.objects.filter(user = request.user)[0] == ((decks.objects.filter(id = id))[0]).deck_creator:
-            if request.method == 'POST':
-                form = add_card_to_deck(request.POST)
-                if form.is_valid():
-                    #handel form
-                    card_name = form.cleaned_data.get("card_name")
-                    quantity = form.cleaned_data.get("quantity")
-                    try:
-                        card_added = (cards.objects.filter(name=card_name))[0]
-                    except:
-                        card_added = False 
-                    if not card_added == False:#should probably return else not a card
-                        new_card = cards_in_deck(card=card_added, deck=deck1, is_commander=False, is_sideboard=False)        
-                        for i in range(quantity): 
-                            new_card.save()
-                        return redirect('http://localhost:8000/deck_search/deck_page/' + str(id))
-                    # end handel form 
-
-            form = add_card_to_deck()
-
-            context.update({'form':form})
-
-            return render(request, 'deck_search/my_deck_page.html', context)
-        else:
-            return render(request, 'deck_search/user_deck_page.html', context)
-    else:
-        return render(request, 'deck_search/deck_page.html', context)
 
 #under this are deck page functions
+
+class deck_page1(View):
+
+    def get(self, request, id):
+        deck = decks.objects.filter(id = id)
+        card2 = cards_in_deck.objects.filter(deck = deck[0]).exclude(is_commander = True).exclude(is_sideboard=True)
+        hash_tags1 =[i.hash_tags for i in deck] 
+        tags = list([(i[1:]).split('#') for i in hash_tags1])
+        if len(tags) == 1:
+            tags = tags[0]
+        card1 = []
+        for i in card2:
+            j = i.card
+            card1.append(j)
+    
+        card3 = list((collections.Counter(card1)).items())
+        #can change type list to any othey card atribute to change how the cards are sorted
+        type_list = [x[0].type1 for x in card3]
+        type_list = list(dict.fromkeys(type_list))
+        #sort_list = sort_function 
+
+        cards_split = []
+        for i in type_list:
+            x = [x for x in card3 if x[0].type1 == i]
+            y = sum([y[1] for y in x ])
+            cards_split.append([i,y,x])
+        #broken up baced on kind of could possibly be more efficent 
+        deck_kind = deck[0].deck_type
+        if deck[0].deck_type == 'Commander':
+            try:
+                commander =  (cards_in_deck.objects.filter(deck = deck[0]).filter(is_commander = True))[0].card
+            except:
+                commander = 2
+            sideboard = 1
+            context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':commander, 'sideboard': sideboard }
+
+
+        elif (deck[0]).deck_type == 'Constructed':
+            try:
+                side = [(i.card) for i in cards_in_deck.objects.filter(deck = deck[0]).filter(is_sideboard= True)]
+                sideboard = list((collections.Counter(side)).items())
+
+            except:
+                sideboard = 2
+            commander = 1
+            context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':commander, 'sideboard': sideboard }
+
+        else:
+            sideboard = 1
+            commander = 1
+            context = {'cards': cards_split, 'id1': id,'tags': tags, 'commander':1, 'sideboard': sideboard }
+
+        try:
+            user = UserProfile.objects.filter(user = request.user)[0]
+        except:
+            user = 1
+
+        deck1 = (decks.objects.get(id=id))
+        if not user == deck1.deck_creator:
+            deck1.deck_views = deck1.deck_views + 1
+            deck1.save(update_fields=['deck_views'])
+
+        form = add_card_to_deck()
+        context.update({'form':form})
+        if request.user.is_authenticated == True:
+            if UserProfile.objects.filter(user = request.user)[0] == ((decks.objects.filter(id = id))[0]).deck_creator:
+                return render(request, 'deck_search/my_deck_page.html', context)
+            else:
+                return render(request, 'deck_search/user_deck_page.html', context)
+        else:
+            return render(request, 'deck_search/deck_page.html', context)
+    
+    def post(self, request, id):
+        deck1 = (decks.objects.get(id=id))
+        form = add_card_to_deck(request.POST)
+        if form.is_valid():
+                    #handel form
+            card_name = form.cleaned_data.get("card_name")
+            quantity = form.cleaned_data.get("quantity")
+            try:
+                card_added = (cards.objects.filter(name=card_name))[0]
+            except:
+                card_added = False 
+            if not card_added == False:#should probably return else not a card        
+                for i in range(quantity):
+                    new_card = cards_in_deck(card=card_added, deck=deck1, is_commander=False, is_sideboard=False)
+                    new_card.save()
+            return redirect('http://localhost:8000/deck_search/deck_page/' + str(id))
+
+
 
 #this function will make a new copie of a published deck that sombody does not have and put it in their decks 
 def copy_deck(request,id):
@@ -226,7 +266,7 @@ def publish_deck_v(request, id):
 #this gives sombody a sample hand of thier deck 
 def sample_hand(request, id):
     deck = (decks.objects.filter(id=id))[0]
-    cards = list(cards_in_deck.objects.filter(deck=deck))
+    cards = list(cards_in_deck.objects.filter(deck=deck).filter(is_commander=False).filter(is_sideboard=False))
     number_of_cards = len(cards) 
     if number_of_cards > 7:
         hand = (random.sample(cards, 7))
@@ -244,6 +284,8 @@ def sample_hand(request, id):
 def popular_decks(request):
     deck_list = decks.objects.filter(deck_publish=True)
     deck_list = deck_list.extra( order_by = ['-deck_copies'])
+    if len(deck_list) > 30:
+        deck_list = deck_list[0:30]
     
     if request.user.is_active == True:
         return render(request, 'deck_search/user_popular_decks.html', {'deck_list': deck_list})
@@ -317,3 +359,75 @@ def remonve_card_from_deck(request,id_c,id_d):
         #sort_list = list(dict.fromkeys(sort_list))
     
     #return(sort_list)
+
+
+def deck_analysis(request,d_id):
+
+    deck = (decks.objects.filter(id=d_id))[0]
+    cards_deck = [i.card for i in cards_in_deck.objects.filter(deck__exact=deck)]
+
+    total_cmc = 0 
+    for i in cards_deck:
+        total_cmc = total_cmc + i.convertedManaCost
+    mana_needed = float(total_cmc)/4.9
+    non_land_cards= [i for i in cards_deck if not i.type1 == 'land']
+
+
+    white_mana_symbols = 0
+    blue_mana_symbols = 0
+    green_mana_symbols = 0
+    black_mana_symbols = 0
+    red_mana_symbols = 0
+    colorless_mana_symbols = 0
+    total_mana_symbols = 0 
+    #need to change color list for non normal mana symbols
+    #color_list = ['W', 'U', 'G'] iterating through something like this will probably be more efficent/ good looking
+
+    for i in non_land_cards:
+        mana_cost = i.manaCost
+        mana_cost = (mana_cost[1:-1]).split('}{')
+        #the mana_cost calculation might need to be changed a little 
+        for j in mana_cost:
+            if 'W' in j:
+                white_mana_symbols += 1
+                total_mana_symbols += 1
+            if 'U' in j:
+                blue_mana_symbols += 1
+                total_mana_symbols += 1
+            if 'G' in j:
+                green_mana_symbols += 1
+                total_mana_symbols += 1
+            if 'B' in j:
+                black_mana_symbols += 1
+                total_mana_symbols += 1
+            if 'R' in j:
+                red_mana_symbols += 1
+                total_mana_symbols += 1
+            if 'C' in j:
+                colorless_mana_symbols += 1
+                total_mana_symbols += 1
+
+    if not total_mana_symbols == 0:
+        white_recomended = round(((white_mana_symbols / total_mana_symbols) * mana_needed), 1)
+        blue_recomended  = round(((blue_mana_symbols / total_mana_symbols) * mana_needed), 1)
+        green_recomended  = round(((green_mana_symbols / total_mana_symbols) * mana_needed), 1)
+        black_recomended  = round(((black_mana_symbols / total_mana_symbols) * mana_needed), 1)
+        red_recomended  = round(((red_mana_symbols / total_mana_symbols) * mana_needed), 1)
+        colorless_recomended  = round(((colorless_mana_symbols / total_mana_symbols) * mana_needed), 1)
+    else:
+        white_recomended = 0
+        blue_recomended  = 0
+        green_recomended  = 0
+        black_recomended  = 0
+        red_recomended  = 0
+        colorless_recomended  = 0
+
+    mana_needed = round(mana_needed,1)
+    average_mana_cost = total_cmc/len(non_land_cards)
+
+    context = {'white_recomended': white_recomended, 'blue_recomended': blue_recomended, 'green_recomended': green_recomended,'black_recomended':black_recomended,'red_recomended':red_recomended, 'colorless_recomended': colorless_recomended, 'average_mana_cost': average_mana_cost, 'mana_needed': mana_needed, 'id':d_id }
+    
+    if request.user.is_active == True:
+        return render(request, 'deck_search/user_deck_analysis.html', context)
+    else:
+        return render(request, 'deck_search/deck_analysis.html', context)
